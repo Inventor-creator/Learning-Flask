a50cd6874fb41ede2d3da20c940129883827aa92
Inventor-creator <aryapat1234@gmail.com>
2025-03-13 01:35:55 +0530

    clearing stuff

--- niggas/algs.py
+++
@@ -1,8 +0,0 @@
-def binarySearchStr(tocheck , list , param1 , param2 , pointer):
-    if tocheck[pointer] < list[(param1 + param2)// 2]:
-        binarySearchStr(tocheck , list , param1,(param1 + param2)// 2 , pointer )
-    elif tocheck[pointer] > list[(param1 + param2)// 2]
-        binarySearchStr(tocheck , list , (param1 + param2)// 2, param2 , pointer)
-    else:
-        binarySearchStr(tocheck , list , param1 , param2 , pointer + 1)
-    
